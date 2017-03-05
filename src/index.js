e741fae1ebbf5911e611d6d262853d3549ffaa21
keilin.olsen <keilin.olsen@heathwallace.com>
2017-03-05 19:11:47 +1100

    compilation added, some issues with function scope

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const
 			selectN = array_utils.selectN,
 			shuffle = array_utils.shuffle;
 
+
 function generateTestData(count) {
 	function generateDatum() {
 		return {
@@ -16,7 +17,8 @@ function generateTestData(count) {
 					: x === 'd' ? select('ts'.split(''))
 					: x
 				),
-			friends: []
+			friends: [],
+			happy: Math.random() < 0.5 ? true : false
 		};
 	}
 
@@ -56,3 +58,100 @@ let fixed = testData
 
 console.log(chalk.red('\n\n## test data with fixed friends ##\n'));
 console.log(fixed);
+
+/**
+ * Actual body of stuff
+ */
+
+let happy = ent => ent.happy;
+let capitalise = str => String.prototype.toUpperCase.call('' + str);
+let capitaliseName = ent => { ent.name = capitalise(ent.name); return ent; };
+let capitaliseFriends = ent => { ent.friends = ent.friends.map(capitalise); return ent; }
+
+let happyCapitals = testData.filter(happy).map(capitaliseName).map(capitaliseFriends);
+
+console.log(chalk.green('\n\n## Normal approach: happy capitals ##\n'));
+console.log(happyCapitals);
+
+class Lazify {
+	constructor(arr) {
+		this.arr = arr;
+		this.actions = [];
+	}
+	filter(fn) {
+		this.actions.push(['filter', fn]);
+		return this;
+	}
+	map(fn) {
+		this.actions.push(['map', fn]);
+		return this;
+	}
+	run() {
+		console.log(chalk.yellow('running'));
+		console.log(this.actions);
+		let actionFunc = this._compile();
+		console.log(actionFunc.toString());
+		return actionFunc(this.arr);
+	}
+	_compile() {
+		let result = [];
+		result = this.actions.map( actionSet => {
+			let action = actionSet[0],
+					fn = actionSet[1];
+			let result = {
+				fn: fn,
+				src: ''
+			};
+			// assumes '$$' is current item, $! is current function
+			switch (action) {
+				case 'filter': result.src = `
+					if (! $!($$) ) { continue; }
+				`; break;
+				case 'map': result.src = `
+					$$ = $!($$);
+				`; break;
+			}
+			return result;
+		});
+
+		// predefine functions
+		let src = [`'use strict'`];
+		src = src.concat( result.map( (entry, idx) => {
+			let funcName = `__func__${idx}__`;
+			let funcSource = entry.fn.toString();
+			let funcIsLambda = funcSource.indexOf('=>') !== -1;
+			entry.funcName = funcName;
+			return `let ${funcName} = ${funcSource}; // ${funcIsLambda ? 'lambda' : 'normal'}`;
+		}));
+
+		// function (inArray)
+		src .push( `			
+			let __result_array__ = [];
+			const len = inArray.length;
+			for (var i=0; i < len; i++) {
+				let __current_item__ = inArray[i];
+		`);
+
+		src = src.concat( result.map( entry => {
+			let src = entry.src
+				.replace(/\$\!/g, entry.funcName)
+				.replace(/\$\$/g, '__current_item__');
+			return src;
+		}));
+
+		src.push( `
+				__result_array__.push(__current_item__);
+			}
+			return __result_array__;
+		`);
+
+		// console.log(chalk.cyan(src.join('\n')));
+		return new Function('inArray', src.join('\n'));
+	}
+}
+
+let lazy = new Lazify(testData)
+	.filter(happy)
+	.map(capitaliseName)
+	.map(capitaliseFriends)
+	.run();
